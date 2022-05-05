3e53a546dcdb3cd36d8c975918590fe77e8ba3a1
Tony Wang <twang6@mit.edu>
2022-05-05 15:06:36 -0400

    More n_trains

--- src/experiments/harmonics/scripts/go.sh
+++ src/experiments/harmonics/scripts/go.sh
@@ -3,7 +3,7 @@
 # This script should be launched from the root of the repository, i.e.
 # ./src/experiments/harmonics/scripts/go.sh
 
-n_trains=(32 50 100 200 400 800 1600 3200 6400 10000 20000 40000 80000)
+n_trains=(32 50 80 100 128 200 400 800 1600 3200 6400 10000 20000 40000 80000)
 regs=(MCLS NONE)
 for n_train in "${n_trains[@]}"
 do
