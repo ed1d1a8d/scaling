3f861fc3ccd5cab41ab9f5130a697f02bd0c1bd1
Igor Zablotchi <igor.zablotchi@gmail.com>
2022-05-11 22:18:58 -0400

    Added matlab code to plot interpolation cliff

---
+++ src/interpolation.m
@@ -0,0 +1,67 @@
+% Create "continuous time" signal, Fc >> f
+Fc = 1e6; % very high sample rate to simulate "continuous time"
+Tc = 1/Fc; % sampling period
+t = (-1:Tc:1)'; % time axis
+f = 10; % signal frequency
+xc = sin(2*pi*f*t); % "continuous time" signal
+
+MAX_SAMPLE_RATE = 100;
+rs=1:MAX_SAMPLE_RATE;
+errors = 0*rs;
+
+for r=rs
+    % Create sampled signal
+    Fs = r; % sampling rate
+    Ts = 1/Fs; % sampling period
+    ratio = round(Ts/Tc);
+    tn = t(1:ratio:end); % sampled time axis
+    xn = xc(1:ratio:end); % sampled signal
+    
+    % % Plot the CT signal and sampled signal
+    % figure
+    % hold on
+    % grid on
+    % plot(t, xc)
+    % stem(tn, xn, 'o')
+    % legend('"Continuous time signal"', 'Sampled signal')
+    
+    % Create and plot sinc train
+    sincTrain = zeros(length(t), length(xn)+1);
+    nind = 1;
+    % figure
+    % cmap = colormap(jet(length(-floor(length(xn)/2):floor(length(xn)/2))));
+    % ax = axes('colororder', cmap);
+    % hold on
+    % grid on
+    
+    % plot(t, xc, 'k', 'LineWidth', 3)
+    for n = -floor(length(xn)/2):floor(length(xn)/2)
+       sincTrain(:, nind) = xn(nind)*sinc((t - n*Ts)/Ts);
+    %    p = plot(t, sincTrain(:, nind), 'LineWidth', 2);
+    %    stem(tn(nind), xn(nind), 'Color', p.Color, 'LineWidth', 2)
+       nind = nind + 1;
+       if (nind > length(xn))
+           break
+       end
+    end
+    % xlabel('t')
+    % ylabel('Amplitude')
+    % set(gca, 'FontSize', 20, 'LineWidth', 3, 'FontWeight', 'bold')
+    
+    xr = sum(sincTrain, 2); % sum(sincTrain, 2) is the interpolated/reconstructed signal, should be equal to xc
+    
+    % figure
+    % hold on
+    % grid on
+    % plot(t, xc)
+    % plot(t, xr) 
+    
+    errors(r) = mean(abs(xc - xr).^2);
+end
+
+figure
+hold on
+grid on
+set(gca, 'YScale', 'log')
+set(gca, 'XScale', 'log')
+plot(rs,errors)
