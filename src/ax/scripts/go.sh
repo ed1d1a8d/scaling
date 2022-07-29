72017cac66b1958ae2c61804259b148cfb6771d4
Tony Wang <twang6@mit.edu>
2022-07-28 20:02:03 -0400

    Add CIFAR10 experiment

--- src/ax/scripts/go.sh
+++ src/ax/scripts/go.sh
@@ -2,18 +2,22 @@
 # Highest level experiment script.
 # This script should be launched from the root of the repository, i.e.
 # ./src/ax/scripts/go.sh
+n_trains=(50 80 100 200 300 500 1000 2000 5000 10000 20000 50000)
+widths=(10 20)
+for width in "${widths[@]}"; do
+  for n_train in "${n_trains[@]}"; do
+    ./src/ax/scripts/slurm.sh \
+      --dataset CIFAR10 \
+      --width $width \
+      --n_train $n_train \
+      --do_adv_training False \
+      --tags try1.3
 
-n_trains=(10000 50000 500000 5942688)
-for n_train in "${n_trains[@]}"; do
-  ./src/ax/scripts/slurm.sh \
-    --n_train $n_train \
-    --do_adv_training False \
-    --width 20 \
-    --tags try1.2.1
-
-  ./src/ax/scripts/slurm.sh \
-    --n_train $n_train \
-    --do_adv_training True \
-    --width 20 \
-    --tags try1.2.1
+    ./src/ax/scripts/slurm.sh \
+      --dataset CIFAR10 \
+      --width $width \
+      --n_train $n_train \
+      --do_adv_training True \
+      --tags try1.3
+  done
 done
