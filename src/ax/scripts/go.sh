7275033c944ca9ea3b7782821fd0aaf8d75e44f5
Tony Wang <twang6@mit.edu>
2022-08-08 02:22:16 -0400

    Set up vit experiment

--- src/ax/scripts/go.sh
+++ src/ax/scripts/go.sh
@@ -2,16 +2,16 @@
 # Highest level experiment script.
 # This script should be launched from the root of the repository, i.e.
 # ./src/ax/scripts/go.sh
-n_trains=(2000 5000 10000 20000 50000 200000)
-adv_eps_trains=(4 6 10 12 14 16)
+# 200 300 500 1000 2000 5000 10000 20000 50000 200000 500000 2000000 5942688
+n_trains=(50000 200000 500000 2000000 5942688)
+adv_trains=(False True)
 for n_train in "${n_trains[@]}"; do
-  for adv_eps_train in "${adv_eps_trains[@]}"; do
+  for adv_train in "${adv_trains[@]}"; do
     ./src/ax/scripts/slurm.sh \
       --dataset CIFAR5m \
-      --width 10 \
-      --do_adv_training True \
+      --model VisionTransformer \
       --n_train $n_train \
-      --adv_eps_train $(echo $adv_eps_train / 255 | bc -l) \
-      --tags eps-ablation-v1
+      --do_adv_training $adv_train \
+      --tags vit-v0
   done
 done
