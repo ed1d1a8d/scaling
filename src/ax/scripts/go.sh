e7a6354dc04627df7e0d6a8206b266bf2dc6e713
Tony Wang <twang6@mit.edu>
2022-08-16 15:29:28 -0400

    Set up script for student-teacher experiments

--- src/ax/scripts/go.sh
+++ src/ax/scripts/go.sh
@@ -3,15 +3,15 @@
 # This script should be launched from the root of the repository, i.e.
 # ./src/ax/scripts/go.sh
 # 200 300 500 1000 2000 5000 10000 20000 50000 200000 500000 2000000 5942688
-n_trains=(50000 200000 500000 2000000 5942688)
+n_trains=(1000 2000 5000 10000 20000 50000 200000 500000 2000000 5942688)
 adv_trains=(False True)
 for n_train in "${n_trains[@]}"; do
   for adv_train in "${adv_trains[@]}"; do
     ./src/ax/scripts/slurm.sh \
       --dataset CIFAR5m \
-      --model VisionTransformer \
+      --teacher_ckpt_path /home/gridsan/groups/ccg/wandb/run-reconstructed-1s50k73h/files/model.ckpt \
       --n_train $n_train \
       --do_adv_training $adv_train \
-      --tags vit-v0
+      --tags student-teacher-v0
   done
 done
