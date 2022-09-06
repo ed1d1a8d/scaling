962f317a9dd117c934a8b2bad3b4c0e09aa99f8a
Tony Wang <twang6@mit.edu>
2022-09-06 18:58:12 -0400

    Bump parallelism

--- src/student_teacher_v2/scripts/go.sh
+++ src/student_teacher_v2/scripts/go.sh
@@ -3,7 +3,7 @@
 # This script should be launched from the root of the repository, i.e.
 # ./src/student_teacher_v2/scripts/go.sh
 
-width_scales=(1 10 100 1000)
+width_scales=(1 5 10 20 100 1000)
 for width_scale in "${width_scales[@]}"; do
   ./src/student_teacher_v2/scripts/slurm.sh \
     --tags overparam-scaling-4-2-1-try2 \
--- src/student_teacher_v2/scripts/slurm.sh
+++ src/student_teacher_v2/scripts/slurm.sh
@@ -42,8 +42,8 @@ n_trains=(5 10 20 50 100 1000 10000 100000 1000000 10000000)
 student_seeds=(1001 1002 1003 1004 1005 1006 1007 1008 1009)
 for n_train in "\${n_trains[@]}"; do
   for student_seed in "\${student_seeds[@]}"; do
-    # Run 8 experiments in parallel
-    waitUntilNJobsRemain 8
+    # Run 12 experiments in parallel
+    waitUntilNJobsRemain 12
 
     # Run experiment
     { LD_LIBRARY_PATH=/home/gridsan/groups/ccg/envs/scaling-v2/lib python -m src.student_teacher_v2.train --n_train \$n_train --student_seed \$student_seed $@; } &
