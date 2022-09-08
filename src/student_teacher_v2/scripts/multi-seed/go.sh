c5c59def5be5098df20f56d15786c58884c8ca97
Igor Zablotchi <igorz@mit.edu>
2022-09-08 18:02:04 -0400

    Set up local-minima-8-96-192-1

---
+++ src/student_teacher_v2/scripts/multi-seed/go.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Highest level experiment script.
+# This script should be launched from the root of the repository, i.e.
+# ./src/student_teacher_v2/scripts/multi-seed/go.sh
+
+start_seeds=(1000 2000 3000 4000 5000 6000 7000 8000 9000)
+for start_seed in "${start_seeds[@]}"; do
+  end_seed=$(expr $start_seed + 999)
+  ./src/student_teacher_v2/scripts/multi-seed/slurm.sh \
+    $start_seed $end_seed \
+    --tags local-minima-8-96-192-1 \
+    --teacher_seed 101 \
+    --n_train -1 \
+    --n_val 10000 \
+    --n_test 1000000 \
+    --samples_per_eval 100000
+done
---
+++ src/student_teacher_v2/scripts/multi-seed/slurm.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# This script should be launched from the root of the repository, i.e.
+# ./src/student_teacher_v2/scripts/multi-seed/slurm.sh
+#
+# For details on how this script works, see the SLURM guide here:
+# https://supercloud.mit.edu/submitting-jobs.
+#
+# Also see https://stackoverflow.com/a/44168719/1337463.
+
+start_seed=$1
+end_seed=$2
+shift; shift;
+
+sbatch <<EOT
+#!/bin/bash
+# Slurm sbatch options
+#SBATCH -o slurm-logs/student-teacher-v2/log-%j
+#SBATCH -c 20
+
+# Load conda environment.
+# See https://github.com/conda/conda/issues/7980#issuecomment-441358406
+# for details on why we do it this way.
+source /state/partition1/llgrid/pkg/anaconda/anaconda3-2021b/etc/profile.d/conda.sh
+conda activate scaling-v2
+
+# Set up and launch proxy
+source scripts/rand-mallory.sh
+{ mallory -config \$TMP_MALLORY_CONFIG; } &
+
+# Utility function that waits until n background jobs are left running
+# First argument is n
+function waitUntilNJobsRemain() {
+  local n_jobs=\$(jobs -rp | wc -l)
+  echo "Waiting on \$n_jobs background jobs until \$1 jobs left..."
+  while [[ \$n_jobs -gt \$1 ]]; do
+    n_jobs=\$(jobs -rp | wc -l)
+    echo -n "."
+    sleep 1
+  done
+  echo ""
+}
+
+# Experiment loop
+for student_seed in \$(eval echo {$start_seed..$end_seed}); do
+    # Run 12 experiments in parallel
+    waitUntilNJobsRemain 12
+
+    # Run experiment
+    { LD_LIBRARY_PATH=/home/gridsan/groups/ccg/envs/scaling-v2/lib python -m src.student_teacher_v2.train --student_seed \$student_seed $@; } &
+done
+
+# Wait until all experiments finish.
+waitUntilNJobsRemain 1
+
+# Then kill mallory
+pkill -P \$\$
+EOT
