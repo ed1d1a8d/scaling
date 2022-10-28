24df5028076634053dcce72c3a23c38f009d85be
Tony Wang <twang6@mit.edu>
2022-10-28 18:28:16 -0400

    Set up experiment

--- src/pretrain/scripts/go.sh
+++ src/pretrain/scripts/go.sh
@@ -3,10 +3,10 @@
 # This script should be launched from the root of the repository, i.e.
 # ./src/pretrain/scripts/go.sh
 
-n_trains=(10 20 50 100 200 500 1000 2000 5000 10000 20000 45000)
+n_trains=(20 50 100 200 500 1000 2000 5000 10000 20000 45000)
 
 for n_train in "${n_trains[@]}"; do
-  ./src/ax/scripts/go.sh \
+  ./src/pretrain/scripts/slurm.sh \
     --n_train $n_train \
     --tags clip-cifar-finetune
 done
