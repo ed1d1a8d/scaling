31fd3a1e33f711dd5e480766c4effb5dfbe435e9
Tony Wang <twang6@mit.edu>
2022-12-30 00:59:46 -0500

    Add app-gaussian-plot-grid figure

---
+++ src/stylesheets.py
@@ -0,0 +1,22 @@
+ICLR_2023 = {
+    "mplstyle": {
+        "pgf.texsystem": "pdflatex",
+        "font.family": "serif",
+        "font.serif": "Times",
+        "text.usetex": True,
+        "pgf.rcfonts": False,
+        "font.weight": "medium",
+        # "figure.constrained_layout.use": True,
+        "figure.dpi": 240,
+        "font.size": 10,
+        "figure.labelsize": 10,
+        "figure.titlesize": 10,
+        "legend.fontsize": 10,
+        "axes.titlesize": 10,
+        "axes.labelsize": 10,
+        "xtick.labelsize": 9,
+        "ytick.labelsize": 9,
+    },
+    "col1_width": 6.75133,  # In inches
+    "col2_width": 3.25063,  # In inches
+}
