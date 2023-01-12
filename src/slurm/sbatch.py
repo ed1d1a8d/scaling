f7cd11978787d3229fe19dd0e1f9f824aa849a85
Tony Wang <twang6@mit.edu>
2023-01-12 03:27:44 -0500

    Don't set LD_LIBRARY_PATH

--- src/slurm/sbatch.py
+++ src/slurm/sbatch.py
@@ -34,7 +34,6 @@ module load cuda/11.6
 # Setup conda-pack environment.
 source scripts/setup-env.sh -r scaling-v3
 source $DST_ENV_PATH/bin/activate
-export LD_LIBRARY_PATH=$DST_ENV_PATH/lib
 
 # Set up and launch proxy
 source scripts/rand-mallory.sh
