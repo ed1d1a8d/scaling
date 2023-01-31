9e09da7a7c44b936628edda07f617662d0fec78e
Tony Wang <twang6@mit.edu>
2023-01-31 18:48:15 -0500

    Make figure better

--- src/stylesheets.py
+++ src/stylesheets.py
@@ -1,4 +1,4 @@
-ICLR_2023 = {
+ICML_2023 = {
     "mplstyle": {
         "pgf.texsystem": "pdflatex",
         "font.family": "serif",
@@ -19,4 +19,27 @@ ICLR_2023 = {
     },
     "col1_width": 6.75133,  # In inches
     "col2_width": 3.25063,  # In inches
+}
+
+ME_FOMO_2023 = {
+    "mplstyle": {
+        "pgf.texsystem": "pdflatex",
+        "font.family": "serif",
+        "font.serif": "Times",
+        "text.usetex": True,
+        "pgf.rcfonts": False,
+        "font.weight": "medium",
+        # "figure.constrained_layout.use": True,
+        "figure.dpi": 240,
+        "font.size": 10,
+        "figure.labelsize": 10,
+        "figure.titlesize": 10,
+        "legend.fontsize": 10,
+        "axes.titlesize": 10,
+        "axes.labelsize": 10,
+        "xtick.labelsize": 9,
+        "ytick.labelsize": 9,
+    },
+    "col1_width": 5.50107,  # In inches
+    "col2_width": 2.64049,  # In inches
 }
